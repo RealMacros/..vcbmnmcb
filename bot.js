933dddf068e5bc933f0de2ec8ec8e2a766887a11
RealMacros <43798716+RealMacros@users.noreply.github.com>
2018-10-03 04:13:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,52 +1,5 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log('======================================')
-  console.log(Logged in as ${client.user.tag}!);
-  console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log('')
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log('=======================================')
-});
-
-
-
-
-
-client.on('message', message => {
-    if(message.content === '^^DailyForAll'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === '^^AllCredit'){
-        message.channel.send('#credits')
-    }
-});
-
-
-client.on('message', message => {
-    if(message.content === '^^RepForRobben'){
-        message.channel.send('#rep @♚ ★ RoBbEn ★♚')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === '^^RepForBoss'){
-        message.channel.send('#rep @B💎SS.♕  ، ✩')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === '^^RepForAlashaq'){
-        message.channel.send('#rep <@346045919072092161>')
-    }
-});
-
-
 
 client.on('message', message => {
 if (message.content === '!spam') {
@@ -62,24 +15,5 @@ if (message.content === '!spam') {
       }
 });
 
-const prefix = ['.']
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-if (command == "say" ) {
-let rank = message.guild.member(message.author).roles.find('name', '.');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
-
 client.login(process.env.BOT_TOKEN);
+
